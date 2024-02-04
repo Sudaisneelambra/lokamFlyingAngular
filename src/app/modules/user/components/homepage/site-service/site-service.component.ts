32f584ed939751f034c50cebeb5b9251a09ec888
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-02-04 10:20:00 +0530

    website services added

--- src/app/modules/user/components/homepage/site-service/site-service.component.ts
+++ src/app/modules/user/components/homepage/site-service/site-service.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-site-service',
+  selector: 'site-service',
   templateUrl: './site-service.component.html',
   styleUrls: ['./site-service.component.css']
 })
