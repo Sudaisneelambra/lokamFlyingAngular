547fa184f7cb8cacf7299f629ee132ee4be92e18
Sudaisneelambra <sudaisanuneelambra21@gmail.com>
2024-02-21 13:03:05 +0530

    package displaying on process

--- src/app/modules/agency/components/agency-home/package/packagefulldetails/packagefulldetails.component.ts
+++ src/app/modules/agency/components/agency-home/package/packagefulldetails/packagefulldetails.component.ts
@@ -1,5 +1,6 @@
+import { Location } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Subscription } from "rxjs";
 import { agencyService } from "src/app/modules/agency/services/agency.service";
 
@@ -14,17 +15,43 @@ export class PackagefullComponent implements OnInit{
     singlepackagedetails$!:Subscription
     singlepackage!:any
     places: any;
+    fulldetails:any
+    objplace:any
+    guide:any
+    result:any[]=[]
+    expiry: any;
     
-    constructor(private rout:ActivatedRoute ,private service:agencyService){}
+    constructor(private rout:ActivatedRoute ,private service:agencyService, private router:Router ,private location:Location){}
     ngOnInit(): void {
         this.rout.params.subscribe(params=>{
             const id=params['id']
             this.singlepackagedetails$=this.service.getsinglepackage(id).subscribe({
                 next:(res)=>{
+                    if(res.expiry){
+                        console.log(res.expiry);
+                        this.expiry=res.expiry          
+                      }  
                     this.singlepackage=res.package
                     this.places=res.place
-                    console.log(this.singlepackage);
-                    console.log(this.places);  
+                    this.objplace=this.singlepackage.places
+                    this.guide=res.guide
+                    console.log(this.objplace);
+                    console.log(this.places); 
+                    console.log(this.guide);
+                    
+
+                    if (this.objplace.length === this.places.length) {
+                         this.result = [];
+                    
+                        for (let i = 0; i < this.objplace.length; i++) {
+                            let joinedObject = {...this.objplace[i], ...this.places[i]};
+                            this.result.push(joinedObject);
+                        }
+                    
+                        console.log(this.result);
+                    } else {
+                        console.log("Arrays a and b must have the same length for joining.");
+                    }
                 },
                 error:(err)=>{
                     console.log(err);
@@ -33,5 +60,11 @@ export class PackagefullComponent implements OnInit{
         })
     }
 
-
+    guideget(id:any){
+        console.log(id);
+        this.router.navigate([`/agency/guidedetails/${id}`])
+    }
+    back(){
+        this.location.back()
+    }
 }
